7e361de2d3cb47bb7462f79700ddd1244dcf1efa
Ariel Sol <aribschwartz@gmail.com>
2026-09-12 18:28:37 -0700

    16 bit audio file

--- global_vars.py
+++ global_vars.py
@@ -9,7 +9,7 @@ DETECTION_COOLDOWN = 0.5    # How often to check for presence, in seconds
 
 # === AUDIO ===
 AUDIO_FILE = "/home/mg/cocooned-together/audio/CocoonRemixEdit_260912.wav" # File path of audio file to play
-TRACK_DURATION = 360    # Duration of audio file, in seconds - better to estimate SHORTER than longer
+TRACK_DURATION = 365    # Duration of audio file, in seconds - better to estimate SHORTER than longer
 MAX_VOLUME = 1.0        # Full volume scale (0.0 to 1.0)
 FADEIN_TIME = 0.2       # Duration of fade-in, in seconds
 FADEOUT_TIME = 1.5      # Duration of fade-out, in seconds
@@ -18,7 +18,7 @@ RESET_TIME = 3600       # Resets playback start time to beginning of file after 
 # === LIGHTS ===
 LED_COUNT = 100                 # Number of LEDs on strip
 LED_BRIGHTNESS = 200            # Range: [0, 255]
-LED_COLOR = (255, 0, 0)         # [0, 255] for R, G, B // warm white: (255, 180, 60) // Warm red: (255, 172, 184)
+LED_COLOR = (255, 180, 60)      # [0, 255] for R, G, B // warm white: (255, 180, 60) // Warm red: (255, 172, 184)
 LED_FADE_S = 5                  # Duration of fade, in seconds
 FADE_STEPS = 100                # Resolution of fade, in number of discrete steps
 SHOW_PULSE = True               # If true, plays animation visualizing data below
